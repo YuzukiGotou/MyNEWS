6d3b3e2f36784a54985d1e6a2713c7ead02f5a60
yuzu <139536156+YuzukiGotou@users.noreply.github.com>
2026-04-18 01:25:17 +0900

    Initial commit

---
+++ mynews/src/test/java/com/yuzu/mynews/MynewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuzu.mynews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
